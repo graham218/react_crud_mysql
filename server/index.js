39fe75b1039158a4e834afe85988f0e656c98cb5
BILL GRAHAM <grahambill011@gmail.com>
2021-10-06 00:46:49 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,15 @@ const db=mysql.createConnection({
     host: "localhost",
     password: "",
     database: "employee_system"
-})
+});
+
+app.post('/create',(req, res)=>{
+    const name=req.body.name;
+    const age=req.body.age;
+    const country=req.body.country;
+    const position=req.body.position;
+    const wage=req.body.wage;
+});
 
 app.listen(3001, ()=>{
     console.log("Hey, your server is running on port 3001")
