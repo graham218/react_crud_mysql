2bafe724522e31570788c79698c3d7e1c148cbf9
BILL GRAHAM <grahambill011@gmail.com>
2021-10-05 23:41:36 +0300

    use states to send data into database

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName]=useState("")
+  const [age, setAge]=useState("")
+  const [country, setCountry]=useState("")
+  const [position, setPosition]=useState("")
+  const [wage, setWage]=useState("")  
   return (
     <div className="App">
       <div className="information">
@@ -14,6 +21,7 @@ function App() {
         <input type="text"/>
         <label>Wage (Year):</label>
         <input type="number"/>
+        <button>Add Employee</button>
       </div>
     </div>
   );
