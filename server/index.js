20e656a4b70a8305b72ad318e74487d2cb682227
BILL GRAHAM <grahambill011@gmail.com>
2021-10-06 00:33:45 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express= require("express");
 const app=express();
 const mysql=require('mysql');
+const db=mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "employee_system"
+})
 
 app.listen(3001, ()=>{
     console.log("Hey, your server is running on port 3001")
