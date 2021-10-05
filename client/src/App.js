1fe9e38696ceea5100cfdd326530bc23cdf37949
BILL GRAHAM <grahambill011@gmail.com>
2021-10-05 23:12:30 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div class="information">
+        <label>Name:</label>
+        <input type="text"/>
+        <label>Age:</label>
+        <input type="number"/>
+        <label>Country:</label>
+        <input type="text"/>
+        <label>Position:</label>
+        <input type="text"/>
+        <label>Wage (Year):</label>
+        <input type="number"/>
+      </div>
     </div>
   );
 }
